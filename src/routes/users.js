94431ee43e3e3ce9152639aa5f2d8813567e8cd3
Abel Belay <abel.belay01@gmail.com>
2021-12-09 03:33:21 -0700

    Add user register feature.

---
+++ src/routes/users.js
@@ -0,0 +1,11 @@
+let express = require("express");
+let router = express.Router();
+
+let userController = require("../controllers/user");
+
+// @route POST /api/user/register
+// @desc Register User
+// @access Public
+router.post("/signup", userController.signup);
+
+module.exports = router;

622b8643afc24d588b93a8b2460b1cd23bf5ce13
Sergio Silva <sergiobtos@hotmail.com>
2021-12-20 14:44:38 -0500

    Hair6_Hair8_SearchUI Complete
    After the user enters a city, if businesses have been found, then move to show the list of Business results

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,34 +1,64 @@
-import React from "react";
+import React, { useState } from 'react'
+import { useHistory } from 'react-router-dom'
 
-import ContentHeader from "../components/ContentHeader";
+import ContentHeader from '../components/ContentHeader'
 
 const Home = () => {
+  const history = useHistory()
+  const [city, setCity] = useState('')
+
+  const handleChange = (e: any) => {
+    if (e.target.value !== '') {
+      setCity(e.target.value.toLowerCase())
+    } else {
+      console.log('Error on handleChange')
+    }
+  }
+
+  const itemSubmitHandler = async (event: React.FormEvent<any>) => {
+    event.preventDefault()
+    try {
+      history.push(`/items/${city}`)
+      console.log(city)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
   return (
     <React.Fragment>
       <div className="content-wrapper">
         <ContentHeader title="Home" />
-        <div className="card w-50 mx-auto">
-          <div className="card-header">
-            <h5 className="m-0">Home stuff</h5>
+        <section className="content">
+          <div className="container-fluid">
+            <h2 className="text-center display-4">
+              Im looking for hairstylist in
+            </h2>
+            <div className="row">
+              <div className="col-md-8 offset-md-2">
+                <form onSubmit={itemSubmitHandler}>
+                  <div className="input-group">
+                    <input
+                      type="search"
+                      value={city}
+                      onChange={handleChange}
+                      className="form-control form-control-lg"
+                      placeholder="Type the city name here"
+                    />
+                    <div className="input-group-append">
+                      <button type="submit" className="btn btn-lg btn-default">
+                        <i className="fa fa-search"></i>
+                      </button>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
           </div>
-          <div className="card-body">
-            <h6 className="card-title">This is the home page</h6>
-
-            <p className="card-text">
-              Sed lobortis magna quis lectus vulputate ornare. Nunc est velit,
-              pharetra sit amet ex in, molestie egestas ex. In ac urna vel purus
-              accumsan vehicula ut a nisi. Fusce vel dui lacus. Vestibulum ante
-              ipsum primis in faucibus orci luctus et ultrices posuere cubilia
-              curae.
-            </p>
-            <a href="#l" className="btn btn-primary">
-              Do Something
-            </a>
-          </div>
-        </div>
+        </section>
       </div>
     </React.Fragment>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home

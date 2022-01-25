5280b4436dc4000d8ba0bade33aa41f6ba40a036
Sergio Silva <sergiobtos@hotmail.com>
2022-01-25 09:23:55 -0500

    Update UserSchema Profile Link Field

--- src/models/users.ts
+++ src/models/users.ts
@@ -1,18 +1,23 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose'
 
-const Schema = mongoose.Schema;
+const Schema = mongoose.Schema
 
 const userSchema = new Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
-  role: { type: String, enum: ["user", "owner"], required: true },
+  role: {
+    type: String,
+    enum: ['user', 'owner'],
+    required: true
+  },
   createdAt: {
     type: Date,
-    default: new Date(),
+    default: new Date()
   },
-});
+  imageProfile: { type: String, required: true}
+})
 
-const User = mongoose.model("User", userSchema);
+const User = mongoose.model('User', userSchema)
 
-module.exports = User;
+module.exports = User

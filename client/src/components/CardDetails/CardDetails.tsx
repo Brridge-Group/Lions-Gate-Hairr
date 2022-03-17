42b1ff892807c8be403017e914bbce63fca258f7
CodeMeKathy * <Kathy@KathyLambert.me>
2022-03-17 17:59:22 -0400

    Feature: Add <CardDetails/> with styles

---
+++ client/src/components/CardDetails/CardDetails.tsx
@@ -0,0 +1,43 @@
+// Custom Styles
+import './CardDetails.css'
+
+// Custom Imports
+import Star from '../../UIElements/Star'
+
+// Types
+interface Props {
+  name: string
+  description: string
+  image: string
+  stars: number
+  address: {
+    street: string
+    city: string
+    region: string
+    postalCode: string
+  }
+}
+
+const CardDetails = (props: Props) => {
+  return (
+    <section className='CardDetails-Container'>
+      <figure className='CardDetails-FigureContainer'>
+        <img className='CardDetails-Image' src={props.image} alt='Business Image' />
+        <Star stars={props.stars} />
+      </figure>
+      <div className='CardDetails-Info'>
+        <h2 className='CardDetails-Name'>{props.name}</h2>
+        <p className='CardDetails-Description'>{props.description}</p>
+        <div className='CardDetails-Address'>
+          <p>{props.address.street}</p>
+          <p>
+            {props.address.city}, {props.address.region}
+          </p>
+          <p>{props.address.postalCode}</p>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default CardDetails

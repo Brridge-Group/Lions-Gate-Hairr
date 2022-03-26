fb73d2aeb29e81f2296a38d7fff4c860ce978512
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2022-03-26 15:15:39 -0400

    profile user placement

--- client/src/pages/Profile/Profile.tsx
+++ client/src/pages/Profile/Profile.tsx
@@ -1,13 +1,36 @@
+import { Result } from 'express-validator'
+import { useState } from 'react'
 import hairgreysm from '../../assets/images/hairgreysm.jpg'
+import MyBusinessList from '../../components/Businesses/MyBusinessList'
 import './Profile.css'
 
 const Profile = () => {
+  const user = JSON.parse(localStorage.getItem('profile') ?? 'false').result
+
+  console.log(user.role)
   return (
-    <div
-      className='Profile-Container'
-      style={{
-        backgroundImage: `url(${hairgreysm})`,
-      }}></div>
+    <>
+      <div
+        className='Profile-Container'
+        style={{
+          backgroundImage: `url(${hairgreysm})`,
+        }}>
+        {user.role === 'user' ? (
+          <div className='Profile-Container_user'>
+            <h1 className='Profile-Container_name'>Hello {user.name} !</h1>
+            <div className='Profile-Container_user-container'>
+              <div
+                className='user-pix-placeholder'
+                style={{ fontSize: '15px' }}>
+                user foto
+              </div>
+            </div>
+          </div>
+        ) : (
+          <MyBusinessList />
+        )}
+      </div>
+    </>
   )
 }
 

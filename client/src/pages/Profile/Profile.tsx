00773db6cce9c4a1a898e019b21595176f93afa8
Sergio Silva <sergiobtos@hotmail.com>
2022-05-09 16:44:24 -0700

    Add Business Image Implemented

--- client/src/pages/Profile/Profile.tsx
+++ client/src/pages/Profile/Profile.tsx
@@ -1,34 +1,34 @@
+import { useHistory, useLocation } from 'react-router-dom'
 import { Link } from 'react-router-dom'
 import { Image } from '@chakra-ui/react'
 import { MyBusinessList } from '../../components/MyBusinessList/MyBusinessList'
 import './Profile.css'
 
 export const Profile = () => {
-  const user = JSON.parse(localStorage.getItem('profile') ?? 'false').result
+  const {role, _id, name, imageProfile} = JSON.parse(localStorage.getItem('profile') || '{}').data.result; 
 
   return (
-    <div
+   <div
       className={
-        user.role === 'user'
+        role === 'user'
           ? 'FeatureContainer_image User'
           : 'FeatureContainer_image Owner'
       }>
       <div className='FeatureContainer'>
-        {user && user.role === 'user' ? (
+        {role && role === 'user' ? (
           <div className='Profile_user'>
-            <h1 className='Profile_name'>Hello {user.name} !</h1>
+            <h1 className='Profile_name'>Hello {name} !</h1>
             <div className='Profile-UserContainer '>
               <div className='' style={{ fontSize: '15px' }}>
               <Image
-                src={user.imageProfile || 'https://imgur.com/LDpwLVZ.jpg'}
-                alt={user.name+'_profilePicture'}
+                src={imageProfile || 'https://imgur.com/LDpwLVZ.jpg'}
+                alt={name+'_profilePicture'}
                 boxSize='125px'
                 borderRadius='25rem'
                 fallbackSrc='https://imgur.com/LDpwLVZ.jpg'
               />
               </div>
               <div className='Profile-UserContainer_reviews'>
-                {/* when reviews are imported here, add loading, setLoading state as in mybusiness component */}
                 <h4>your reviews</h4>
               </div>
               <div className='user-reviews-placeholder'>
@@ -36,7 +36,7 @@ export const Profile = () => {
               </div>
             </div>
             <div className='Profile_links'>
-              <Link to={`users/${user._id}`}>
+              <Link to={`users/${_id}`}>
                 <h6 className='btn--btn-primary'>update profile</h6>
               </Link>
               <Link to={'#'}>

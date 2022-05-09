12f162c9dd01ead2cb6dc259b7df76c7cc68d904
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2022-05-09 15:16:49 -0400

    add navbar mobile

---
+++ client/src/components/dist/NavbarMobile.js
@@ -0,0 +1,7 @@
+"use strict";
+exports.__esModule = true;
+exports.NavbarMobile = void 0;
+var react_1 = require("react");
+exports.NavbarMobile = function () {
+    return react_1["default"].createElement("div", null, "NavbarMobile");
+};

afa977d586ac6cf0abb93ac2f00f7849c705922b
CodeMeKathy * <Kathy@KathyLambert.me>
2022-05-23 19:45:00 -0400

    Fix: Clean up merge conflicts

--- client/src/pages/Auth/EditProfile.tsx
+++ client/src/pages/Auth/EditProfile.tsx
@@ -16,7 +16,7 @@ import 'react-toastify/dist/ReactToastify.css'
 toast.configure()
 
 export const EditProfile = () => {
-  const user = JSON.parse(localStorage.getItem('profile') ?? 'false').data.result
+  const user = JSON.parse(localStorage.getItem('profile') ?? 'false').result
 
   const dispatch = useDispatch()
   const history = useHistory()

55509f0b44b0c11fe0bc863d5ebb40a2b54c5843
CodeMeKathy * <Kathy@KathyLambert.me>
2022-05-23 13:36:48 -0400

    Refactor(CardDetails): Update interface with props name

--- client/src/components/CardDetails/CardDetails.tsx
+++ client/src/components/CardDetails/CardDetails.tsx
@@ -5,39 +5,58 @@ import './CardDetails.css'
 import { Star } from '../../UIElements/Star'
 
 // Types
-interface Props {
+interface Business {
   name: string
   description: string
   image: string
   stars: number
   address: {
     address1: string
+    address2: string
     city: string
     region: string
     postalCode: string
   }
 }
 
-const CardDetails = (props: Props) => {
+interface Business {
+  name: string
+  description: string
+  image: string
+  address: {
+    address1: string
+    address2: string
+    city: string
+    region: string
+    postalCode: string
+  }
+}
+
+export const CardDetails = (business: Business) => {
   return (
     <section className='CardDetails-Container'>
       <figure className='CardDetails-FigureContainer'>
-        <img className='CardDetails-Image' src={props.image} alt='Business Image' />
-        <Star stars={props.stars} />
+        <img
+          className='CardDetails-Image'
+          src={business.image}
+          alt='Business Image'
+        />
+        <Star stars={business.stars} />
       </figure>
       <div className='CardDetails-Info'>
-        <h2 className='CardDetails-Name'>{props.name}</h2>
-        <p className='CardDetails-Description'>{props.description}</p>
+        <h2 className='CardDetails-Name'>{business.name}</h2>
+        <p className='CardDetails-Description'>{business.description}</p>
         <div className='CardDetails-Address'>
-          <p>{props.address.address1}</p>
+          <p>{business.address?.address1}</p>
+          <p>{business.address?.address2}</p>
           <p>
-            {props.address.city}, {props.address.region}
+            {business.address?.city}, {business.address?.region}
           </p>
-          <p>{props.address.postalCode}</p>
+          <p>{business.address?.postalCode}</p>
         </div>
       </div>
     </section>
   )
 }
 
-export default CardDetails
+// export default CardDetails

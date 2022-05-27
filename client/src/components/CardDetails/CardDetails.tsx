a6ecf31585a4b054cb02afa10383dea69f77597e
CodeMeKathy * <Kathy@KathyLambert.me>
2022-05-27 11:31:55 -0400

    Fix (CardDetails): Update business data props

--- client/src/components/CardDetails/CardDetails.tsx
+++ client/src/components/CardDetails/CardDetails.tsx
@@ -6,23 +6,10 @@ import { Star } from '../../UIElements/Star'
 
 // Types
 interface Business {
-  name: string
+  businessName: string
   description: string
   image: string
   stars: number
-  address: {
-    address1: string
-    address2: string
-    city: string
-    region: string
-    postalCode: string
-  }
-}
-
-interface Business {
-  name: string
-  description: string
-  image: string
   address: {
     address1: string
     address2: string
@@ -36,15 +23,20 @@ export const CardDetails = (business: Business) => {
   return (
     <section className='CardDetails-Container'>
       <figure className='CardDetails-FigureContainer'>
+        {/* <img className='CardDetails-Image' src={business.image} alt='Business Image' /> */}
         <img
           className='CardDetails-Image'
-          src={business.image}
+          src={
+            business.image == ''
+              ? 'https://images.unsplash.com/photo-1560066984-138dadb4c035?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1674&q=80'
+              : business.image
+          }
           alt='Business Image'
         />
         <Star stars={business.stars} />
       </figure>
       <div className='CardDetails-Info'>
-        <h2 className='CardDetails-Name'>{business.name}</h2>
+        <h2 className='CardDetails-Name'>{business.businessName}</h2>
         <p className='CardDetails-Description'>{business.description}</p>
         <div className='CardDetails-Address'>
           <p>{business.address?.address1}</p>

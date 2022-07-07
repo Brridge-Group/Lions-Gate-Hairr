3b00630879535011c92837b9002ed25844cbbd5d
CodeMeKathy * <Kathy@KathyLambert.me>
2022-07-07 12:24:44 -0400

    Fix: Clean up

--- client/src/UIElements/Card.tsx
+++ client/src/UIElements/Card.tsx
@@ -1,7 +1,7 @@
 import './Card.css'
 
 export const Card = props => {
-  console.log('in card, props', props)
+  // console.log('in card, props', props)
   return (
     <div className={`card ${props.className}`} style={props.style}>
       {props.children}
